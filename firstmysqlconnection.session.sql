31f49ec080bb181cdb96229df747cffb63db0975
t3manish <t3manish@gmail.com>
2024-10-07 01:02:01 +0530

    Update Section 1 done

--- firstmysqlconnection.session.sql
+++ firstmysqlconnection.session.sql
@@ -1,2 +1,20 @@
+-- https://www.geeksforgeeks.org/sql-tutorial/
+
 -- creating a database
 CREATE DATABASE GitHub_MYSQL;
+
+-- DROP DATABASE database_name;
+DROP DATABASE manish1tr;
+
+-- Shows DBs
+SHOW DATABASES;
+
+-- ALTER DATABASE [current_database_name] MODIFY NAME = [new_database_name];
+-- RENAME DATABASE [current_database_name] TO [new_database_name];
+
+USE GitHub_MYSQL;
+-- Why do we need to Select a Database?
+-- ANS: We need to Select a Database to state SQL about the database we will be using for storing the data.
+
+-- ----------------------------------------------------------------------------------------------------
+-- Tables in SQL
